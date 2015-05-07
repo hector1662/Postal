7eae16fcbfa5133c106e9edec4144bc75a5d224e
hector1662@gmail.com <hector1662>
2015-05-07 17:57:12 -0500

    Initial Commit

---
+++ postal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  postal
+//
+//  Created by Héctor Márquez Aguilar on 07/05/15.
+//  Copyright (c) 2015 Asesores en Tecnologías Mixtas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
